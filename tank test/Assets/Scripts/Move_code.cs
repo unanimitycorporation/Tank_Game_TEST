2a08da37b3933f007fc40f31d931f7089c2677af
theenderfun <theenderf@gmail.com>
2018-08-25 19:08:35 +0300

    tank
    ff

---
+++ tank test/Assets/Scripts/Move_code.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_code : MonoBehaviour {
+
+    public float speed = 5;
+    public float rotate = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, Time.deltaTime * rotate,0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, Time.deltaTime * -rotate, 0);
+        }
+    }
+}
---
+++ tank test/Assets/Scripts/Rotate_Turret_Code.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Turret_Code : MonoBehaviour {
+
+    public float rotate = 50;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(0, Time.deltaTime * rotate, 0);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Rotate(0, Time.deltaTime * -rotate, 0);
+        }
+    }
+}
